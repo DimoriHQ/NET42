cb276b7340aa7aa101360aa070277eb41f647918
Khanh <nguyenvana12t2@gmail.com>
2023-10-17 00:34:27 +0700

    unitTest

--- packages/contracts/test/RunningNFT.test.js
+++ packages/contracts/test/RunningNFT.test.js
@@ -28,27 +28,43 @@ describe("RunningNFT", function () {
 
 
   it("Should allow users to claim NFTs", async function () {
-    // Call a function to allow users to claim NFTs and check if they were claimed successfully.
+    // Call a function to allow user1 to claim NFTs and check if they were claimed successfully.
     await runningNFT.createAchievement(21, 300, "IPFS_CID_21KM", "Campaign3");
-    await runningNFT.claimed(2, 21, "Campaign3");
-    const user1NFTBalance = await runningNFT.balanceOf(owner.address);
+    await runningNFT.connect(user1).claimed(2, 21, "Campaign3");
+    const user1NFTBalance = await runningNFT.balanceOf(user1.address);
 
     expect(user1NFTBalance).to.equal(1);
   });
 
   
-  // it("Should not allow users to claim the same NFT twice", async function () {
-  //   // Attempt to claim an NFT again and check if it fails as expected.
-  //   await expect(runningNFT.claimed(2, 5, "Campaign1")).not.to.be.reverted;
-  // });
+  it("Should not allow users to claim the same NFT twice", async function () {
+    // Attempt to claim an NFT again and check if it fails as expected.
+    await expect(runningNFT.connect(user1).claimed(2, 5, "Campaign3")).to.be.reverted;
+  });
 
-  it("Let user can mint another NFT in another Campaign", async function () { //Khánh muốn kiểm tra là ok, Khánh đã mint thành công NFT giải Campaign3, giải đó Khánh có 
-                                                                                    //ID =2. Bây giờ vs giải Campaign1, Khánh có ID 1, cũng muốn mint NFT giải này, thì bị revert?
-    // Attempt to claim an NFT and check if it fails as expected.
-    await expect(runningNFT.claimed(1, 5, "Campaign1")).not.to.be.reverted;
+  it("Let user can mint another NFT in another Campaign", async function () { 
+    await expect(runningNFT.connect(user1).claimed(1, 5, "Campaign1")).not.to.be.reverted;
+  });
+
+  it("Should allow users to claim NFTs", async function () {
+    // Call a function to allow user2 to claim NFTs and check if they were claimed successfully.
+    await runningNFT.createAchievement(42, 300, "IPFS_CID_42KM", "Campaign4");
+    await runningNFT.connect(user2).claimed(2, 52, "Campaign4");
+    const user2NFTBalance = await runningNFT.balanceOf(user2.address);
+
+    expect(user2NFTBalance).to.equal(1);
   });
   
 });
+
+
+
+
+
+
+
+
+
 
 
 
