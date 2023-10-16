09477aceabeac16cbd2fc45cc49b0337f618b8e9
kietkkun191 <kietnguyen.work@gmail.com>
2023-10-16 19:02:43 +0700

    handle json result

--- packages/server/src/models/checkpointsTrack.ts
+++ packages/server/src/models/checkpointsTrack.ts
@@ -1,8 +1,8 @@
 export const checkpoints = async (checkpoints, total_distance) => {
-    let action = "checkpoints: {";
+    let action = 'checkpoints: {';
     for (let idx = 0; idx < checkpoints.length; idx++) {
         action += `\n\t"${checkpoints[idx]}": ${checkpoints[idx] < total_distance},`
     }
-    action += "\n}";
-    return action;
+    action += '\n}';
+    return JSON.parse(JSON.stringify(action));
   };
