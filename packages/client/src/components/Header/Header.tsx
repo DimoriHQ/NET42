3dfb87dcc940499c0535505850af5816baf74da1
kietkun <42093051+kietkun@users.noreply.github.com>
2023-10-18 19:18:08 +0700

    NET-42 UI (#2)
    * tracking strava data
    
    * handle json result
    
    * basicUI: header, footer, hero
    
    * refine landing page, change logo
    
    * update UI
    
    * update profile, nftCard from Smartcontract
    
    ---------
    
    Co-authored-by: kietkkun191 <kietnguyen.work@gmail.com>
    Co-authored-by: vuca <vhpham.lab@gmail.com>

--- packages/client/src/components/Header/Header.tsx
+++ packages/client/src/components/Header/Header.tsx
@@ -1,11 +1,14 @@
 import { disconnect } from "@wagmi/core";
 import { ethers } from "ethers";
 import React from "react";
-import { Link } from "react-router-dom";
 import { useAccount } from "wagmi";
 import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import { clearAuth, selectAuth, setProvider, verify } from "../../features/authentication/reducer";
 import Section from "../Layout/Section";
+import { Box, Button, Link } from "@mui/joy";
+import Typography from '@mui/joy/Typography';
+import "../../styles/header.css";
+// import ColorSchemeToggle from './ColorSchemeToggle';
 
 const Header: React.FC = () => {
   const auth = useAppSelector(selectAuth);
@@ -27,33 +30,74 @@ const Header: React.FC = () => {
 
   const Profile = () => {
     if (isConnected) {
-      return (
+      return (  
         <div>
-          <div>Connected: {address}</div>
           <div>
-            <Link to={`/profile/${address}`}>My profile</Link>
+            <Button color="neutral" variant="solid"><a href={`/profile/${address}`}>My profile: {address.slice(0, 8)}</a></Button>
+            <Button color="danger" onClick={logout}>
+          Logout
+          </Button>
           </div>
-          <button onClick={logout}>Logout</button>
+          
         </div>
       );
     } else {
       return (
-        <button className="" onClick={connect}>
+        <Button color="primary" onClick={connect}>
           Connect
-        </button>
+        </Button>
       );
     }
   };
 
   return (
-    <Section>
-      <header className="flex gap-4 justify-between py-6">
-        <Link to="/">
-          <img src="/images/NET42.png" width={100} />
-        </Link>
-        <div>{Profile()}</div>
+      <header className="header">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          width: '100%',
+          top: 0,
+          px: 1,
+          py: 1,
+          zIndex: 10000,
+          // backgroundColor: 'black',
+          // opacity: 0.8,
+          // borderBottom: '1px solid',
+          borderColor: 'divider',
+          position: 'sticky',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            gap: 1.5,
+          }}
+        >
+          <Link href="/">
+            <img src="/images/n42.png" width={100} />
+          </Link>
+          <Typography component="h1" fontSize="lg" fontWeight="xl" textColor="white">
+            Net Forty-Two
+          </Typography>
+        </Box>
+        <Box sx={{ display: 'flex', flexDirection: 'row', gap: 3 }}>
+          <Box
+            sx={{
+              gap: 1,
+              alignItems: 'center',
+              display: { xs: 'none', sm: 'flex' },
+            }}
+          >
+            <div>{Profile()}</div>
+          </Box>
+        </Box>
+      </Box>
       </header>
-    </Section>
   );
 };
 
--- packages/client/src/components/Home/Hero.tsx
+++ packages/client/src/components/Home/Hero.tsx
@@ -1,32 +1,51 @@
-import BookmarkOutlinedIcon from "@mui/icons-material/BookmarkOutlined";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import Button from "@mui/joy/Button";
-import Card from "@mui/joy/Card";
-import Chip from "@mui/joy/Chip";
-import IconButton from "@mui/joy/IconButton";
-import Typography from "@mui/joy/Typography";
-import React from "react";
+import React, { useRef } from "react";
 import Section from "../Layout/Section";
+import "../../styles/Hero.css";
+import { Card } from "@mui/joy";
 
 const Hero: React.FC = () => {
+  const ref = useRef(null);
+  const handleClick = () => {
+    ref.current?.scrollIntoView({ behavior: "smooth" });
+  };
   return (
-    <Section>
-      hero/Banner
-      <Card sx={{ gap: 2, maxWidth: 300 }}>
-        <Chip size="sm" variant="soft" sx={{ alignSelf: "flex-start", borderRadius: "xl" }}>
-          New
-        </Chip>
-        <IconButton variant="outlined" color="neutral" size="sm" sx={{ position: "absolute", top: "0.75rem", right: "0.75rem" }}>
-          <BookmarkOutlinedIcon />
-        </IconButton>
-        <Typography level="title-lg" fontWeight="lg">
-          Lear more about NET42.run
-        </Typography>
-        <Button variant="solid" endDecorator={<KeyboardArrowRightIcon />}>
-          Read more
-        </Button>
-      </Card>
-    </Section>
+    <div>
+      <section className="hero">
+        <Section>
+          <div className="hero-inner">
+            <h1>NET42</h1>
+            <h2>Learn more about NET42.run</h2>
+            <Button variant="solid" endDecorator={<KeyboardArrowRightIcon />} className="btn" onClick={handleClick}>
+              Read more
+            </Button>
+          </div>
+        </Section>
+      </section>
+      <div ref={ref} className="py-12"><br /></div>
+      <main className="main flex gap-4 justify-between py-6">
+        <Section>
+          <div className="hero-det">
+            <p>
+              Welcome to the world of NFTerraPace, where every step you take not only boosts your health but also fuels a brighter future. We're on a mission to infuse running with
+              purpose and passion, driven by the magic of NFTs and the boundless potential of Web3.
+            </p>
+            <br />
+            <p>
+              Within the fitness community, there is a growing need for motivation, engagement, and sustainability. Traditional fitness apps often lack compelling incentives and a
+              sense of community, particularly in addressing environmental impact and supporting social causes. Meanwhile, runners seek ways to contribute beyond their personal
+              health and turn their passion into a force for good.
+            </p>
+            <br />
+            <p>
+              NFTerraPace tackles these issues head-on. We're building a platform that encourages users to run not only for personal health, but also environmental and social
+              causes. Our secret sauce? NFT-based rewards. Imagine earning unique digital tokens for your running achievements and using them to support social causes.
+            </p>
+          </div>
+        </Section>
+      </main>
+    </div>
   );
 };
 
